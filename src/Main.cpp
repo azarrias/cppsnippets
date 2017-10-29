7de98334634f9326da12cf0b12ed7cf2c91ac469
azarrias <adolfo.zarrias@gmail.com>
2017-10-29 17:43:38 +0100

    Add QuickSort algorithm

--- src/Main.cpp
+++ src/Main.cpp
@@ -13,38 +13,54 @@ using namespace std;
 int LinearSearch(const vector<int> &l, int n);
 int BinarySearch(const vector<int> &l, int i);
 int FindSmallestElement(const vector<int> &l, size_t i = 0);
-int SelectionSort(vector<int> &l);
+void SelectionSort(vector<int> &l);
+void QuickSort(vector<int> &l);
+void QuickSort(vector<int> &l, size_t start, size_t end);
 
 int main(int argc, char *argv[])
 {
+	static const size_t SORTED_VECTOR_SIZE = 10000000;
+	static const size_t TEST_VECTOR_SIZE = 5000;
+
 	// Fill vector with random numbers
 	random_device rnd_device;
 	mt19937 mersenne_engine(rnd_device());
 	uniform_int_distribution<int> dist(0, 9999);
 
 	auto generator = std::bind(dist, mersenne_engine);
-	vector<int> test_vector(3000);
+	vector<int> test_vector(TEST_VECTOR_SIZE);
 	generate(begin(test_vector), end(test_vector), generator);
 
 	// Generate sorted vector
-	vector<int> sorted_vector(100000);
+	vector<int> sorted_vector(SORTED_VECTOR_SIZE);
 	iota(begin(sorted_vector), end(sorted_vector), 0);
 
+	//vector<int> test_vector2 = { 6, 1, 4, 9, 0, 3, 5, 2, 7, 8 };
+	
 	Timer *timer = new Timer();
 	timer->Start();
-	LinearSearch(sorted_vector, 999);
-	cout << "Linear Search: took " << timer->Elapsed() << " milliseconds." << endl;
+	LinearSearch(sorted_vector, 9999999);
+	cout << "Linear Search (" << SORTED_VECTOR_SIZE << " elements): took " 
+		<< timer->Elapsed() << " milliseconds." << endl;
 
 	timer->Start();
-	BinarySearch(sorted_vector, 999);
-	cout << "Binary Search: took " << timer->Elapsed() << " milliseconds." << endl;
+	BinarySearch(sorted_vector, 9999999);
+	cout << "Binary Search (" << SORTED_VECTOR_SIZE << " elements): took " 
+		<< timer->Elapsed() << " milliseconds." << endl;
 
 	timer->Start();
 	SelectionSort(test_vector);
-	cout << "Selection Sort: took " << timer->Elapsed() << " milliseconds." << endl;
+	cout << "Selection Sort (" << TEST_VECTOR_SIZE << " elements): took "
+		<< timer->Elapsed() << " milliseconds." << endl;
 
-	//	for (int x : test_vector)
-	//		cout << x << " ";
+	// Test quick sort
+	timer->Start();
+	QuickSort(test_vector);
+	cout << "Quick Sort (" << TEST_VECTOR_SIZE << " elements): took "
+		<< timer->Elapsed() << " milliseconds." << endl;
+
+	//for (int x : test_vector2)
+	//	cout << x << " ";
 	//	getchar();
 
 	delete(timer);
@@ -52,10 +68,11 @@ int main(int argc, char *argv[])
 }
 
 /**
- * @brief Linear search implementation, with O(n) complexity
+ * @brief Linear search implementation
  * @param const vector<int> & l -> Sorted list of elements
  * @param int n -> Element that we want to find in that list
  * @return int -> Index of the element in the list, -1 if not found
+ * @details Worst-Case: O(n)
  */
 int LinearSearch(const vector<int> &l, int n)
 {
@@ -69,10 +86,11 @@ int LinearSearch(const vector<int> &l, int n)
 }
 
 /**
- * @brief Binary search implementation, with O(log n) complexity
+ * @brief Binary search implementation
  * @param std::vector<int> l -> Sorted list of elements
  * @param int i -> Element that we want to find in that list
  * @return int -> Index of the element in the list, -1 if not found
+ * @details Worst-Case: O(log n)
  */
 int BinarySearch(const vector<int> &l, int i) 
 {
@@ -123,15 +141,13 @@ int FindSmallestElement(const vector<int> &l, size_t i)
 }
 
 /**
- * @brief Selection Sort implementation (ascending), with O(n^2) complexity
+ * @brief Selection Sort implementation (ascending)
  * @param std::vector<int> & l -> List of elements that must be sorted
- * @return int -> 0: success / -1: the list cannot be sorted
+ * @return void
+ * @details Worst-Case: O(n^2)
  */
-int SelectionSort(vector<int> &l)
+void SelectionSort(vector<int> &l)
 {
-	if (l.size() < 1)
-		return -1;
-
 	int smallest_index = 0;
 
 	for (size_t i = 0; i < l.size(); ++i)
@@ -139,6 +155,71 @@ int SelectionSort(vector<int> &l)
 		smallest_index = FindSmallestElement(l, i);
 		swap(l[smallest_index], l[i]);
 	}
+}
 
-	return 0;
+/**
+ * @brief Median of Three for QuickSort partitioning, solves the problem of sorted input
+ * @param vector<int> & l -> List of elements to be sorted
+ * @param size_t left -> Index to the leftmost element of the list
+ * @param size_t right -> Index to the rightmost element of the list
+ * @return int -> Value of the pivot to be used in QuickSort
+ */
+int MedianOfThree(vector<int> &l, size_t left, size_t right)
+{
+	// Sort the left, center and right elements ascending
+	int center = (left + right) / 2;
+	if (l[center] < l[left])
+		swap(l[left], l[center]);
+	if (l[right] < l[left])
+		swap(l[left], l[right]);
+	if (l[right] < l[center])
+		swap(l[center], l[right]);
+
+	// Hide the pivot, by putting it at the end of the container, and return it
+	swap(l[center], l[right]);
+	return l[right];
+}
+
+/**
+ * @brief QuickSort implementation (ascending)
+ * @param vector<int> & l -> List of elements to be sorted
+ * @return void
+ * @details Worst-Case: O(n^2), Average-Case: O(n log n)
+ */
+void QuickSort(vector<int> &l)
+{
+	QuickSort(l, 0, l.size() - 1);
+}
+
+void QuickSort(vector<int> &l, size_t left, size_t right)
+{
+	int pivot;
+	size_t i, j;
+
+	// Base case: Vector with 2 elements
+	if (right - left == 1 && l[right] < l[left])
+		swap(l[left], l[right]);
+	// Recursive case (with more than 2 elements)
+	else if (right - left > 1)
+	{
+		pivot = MedianOfThree(l, left, right);
+		i = left;
+		j = right - 1;
+
+		while (true)
+		{
+			while (l[++i] < pivot);
+			while (pivot < l[--j]);
+			if (i < j)
+				swap(l[i], l[j]);
+			else
+				break;
+		}
+
+		// Restore pivot position after hiding it
+		swap(l[i], l[right]);
+
+		QuickSort(l, left, i - 1);
+		QuickSort(l, i + 1, right);
+	}
 }
